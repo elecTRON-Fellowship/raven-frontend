5654c5c126316efd628d2fa1b8aaa18e39ceed86
Mizanali Panjwani <mizanalip786@gmail.com>
2021-06-11 19:04:22 +0530

    Create user provider

---
+++ lib/providers/user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class User with ChangeNotifier {
+  String firstName = '';
+  String lastName = '';
+  String username = '';
+  String email = '';
+  String authToken = '';
+}
